2b594942b27682b89f1e07ef80ae2ade1ab6408c
dyeghiaz <dyeghiaz@gmail.com>
2023-08-13 22:51:18 +0400

    Initial Commit

---
+++ src/main/java/com/aca/acacourierservice/AcaCourierServiceApplication.java
@@ -0,0 +1,13 @@
+package com.aca.acacourierservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcaCourierServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcaCourierServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aca/acacourierservice/AcaCourierServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aca.acacourierservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcaCourierServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
