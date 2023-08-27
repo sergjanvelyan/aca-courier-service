503058d3a40334344cd4a7aa8aeda212717cba31
YN0728 <nataly.yayloyan@gmail.com>
2023-08-27 17:51:52 +0400

    implemented basic logic for user.Added dependency for SpringSecurity

--- src/main/java/com/aca/acacourierservice/model/UserJson.java
+++ src/main/java/com/aca/acacourierservice/model/UserJson.java
@@ -1,4 +1,71 @@
 package com.aca.acacourierservice.model;
 
+import com.aca.acacourierservice.entity.User;
+
+import java.util.Date;
+
 public class UserJson {
+    private String email;
+    private String password;
+    private User.Role role;
+    private String address;
+    private String phoneNumber;
+    private String fullName;
+    private Date birthDate;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public User.Role getRole() {
+        return role;
+    }
+
+    public void setRole(User.Role role) {
+        this.role = role;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
 }
