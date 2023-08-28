69764c04a8a4ef962962e581bf8763e44036fd20
sergjanvelyan <sergey.janvelyan1999@gmail.com>
2023-08-28 19:36:35 +0400

    created model StatusUpdateTimeJson

--- src/main/java/com/aca/acacourierservice/model/StatusUpdateTimeJson.java
+++ src/main/java/com/aca/acacourierservice/model/StatusUpdateTimeJson.java
@@ -1,4 +1,54 @@
 package com.aca.acacourierservice.model;
 
+import com.aca.acacourierservice.entity.Order;
+import jakarta.persistence.*;
+
+import java.util.Date;
+
 public class StatusUpdateTimeJson {
+    private Date updateTime;
+    private Order.Status updatedFrom;
+    private Order.Status updatedTo;
+    private String additionalInfo;
+    private Order order;
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Order.Status getUpdatedFrom() {
+        return updatedFrom;
+    }
+
+    public void setUpdatedFrom(Order.Status updatedFrom) {
+        this.updatedFrom = updatedFrom;
+    }
+
+    public Order.Status getUpdatedTo() {
+        return updatedTo;
+    }
+
+    public void setUpdatedTo(Order.Status updatedTo) {
+        this.updatedTo = updatedTo;
+    }
+
+    public String getAdditionalInfo() {
+        return additionalInfo;
+    }
+
+    public void setAdditionalInfo(String additionalInfo) {
+        this.additionalInfo = additionalInfo;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
 }
