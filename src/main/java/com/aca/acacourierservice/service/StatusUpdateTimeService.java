5a3c9d2206e60dececafbae15b3c3565c93f34d2
sergjanvelyan <sergey.janvelyan1999@gmail.com>
2023-08-28 20:44:28 +0400

    implemented basic methods in StatusUpdateTimeService

--- src/main/java/com/aca/acacourierservice/service/StatusUpdateTimeService.java
+++ src/main/java/com/aca/acacourierservice/service/StatusUpdateTimeService.java
@@ -1,7 +1,46 @@
 package com.aca.acacourierservice.service;
 
+import com.aca.acacourierservice.converter.StatusUpdateTimeConverter;
+import com.aca.acacourierservice.entity.StatusUpdateTime;
+import com.aca.acacourierservice.exception.CourierServiceException;
+import com.aca.acacourierservice.model.StatusUpdateTimeJson;
+import com.aca.acacourierservice.repository.StatusUpdateTimeRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class StatusUpdateTimeService {
+    private final StatusUpdateTimeRepository statusUpdateTimeRepository;
+    private final StatusUpdateTimeConverter statusUpdateTimeConverter;
+    @Autowired
+    public StatusUpdateTimeService(StatusUpdateTimeRepository statusUpdateTimeRepository, StatusUpdateTimeConverter statusUpdateTimeConverter) {
+        this.statusUpdateTimeRepository = statusUpdateTimeRepository;
+        this.statusUpdateTimeConverter = statusUpdateTimeConverter;
+    }
+    @Transactional
+    public long addStatusUpdateTime(StatusUpdateTimeJson statusUpdateTimeJson){
+        StatusUpdateTime statusUpdateTime = statusUpdateTimeConverter.convertToEntity(statusUpdateTimeJson);
+        return statusUpdateTime.getId();
+    }
+    @Transactional
+    public void updateStatusUpdateTime(long id,StatusUpdateTimeJson statusUpdateTimeJson){
+
+    }
+    public StatusUpdateTime getStatusUpdateTimeById(long id){
+        Optional<StatusUpdateTime> optionalStatusUpdateTime = statusUpdateTimeRepository.findById(id);
+        if(optionalStatusUpdateTime.isEmpty()){
+            throw new CourierServiceException("There is no StatusUpdateTime with id("+id+")");
+        }
+        return optionalStatusUpdateTime.get();
+    }
+    @Transactional
+    public void deleteStatsUpdateTimeById(long id){
+        if(!statusUpdateTimeRepository.existsById(id)){
+            throw new CourierServiceException("There is no StatusUpdateTime with id("+id+")");
+        }
+        statusUpdateTimeRepository.deleteById(id);
+    }
 }
